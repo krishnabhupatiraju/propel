a24296b06c6a62dc0ff5c9c749d2f77123449970
Krishna Bhupatiraju <krishna.bhupatiraju@airbnb.com>
2017-10-08 01:26:40 -0700

    Added tweets model

--- propel/models.py
+++ propel/models.py
@@ -1,69 +1,133 @@
 from datetime import datetime
-from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy import (create_engine, Table, Column, String, 
+                        Integer, DateTime, Enum, ForeignKey)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///:memory:',
+                       echo=True)
 Session = sessionmaker(bind=engine)
 
-class Connection(Base):
+class Connections(Base):
     __tablename__ = 'connections'
-    id = Column(Integer(), primary_key=True)
-    conn_name = Column(String(255), nullable=False)
-    conn_type = Column(String(255))
-    key = Column(String(255))
-    secret = Column(String(255))
-    token = Column(String(255))
-    created_at = Column(DateTime(), default = datetime.now)
-    updated_on = Column(DateTime(),
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    type = Column(String(255))
+    key = Column(String(5000))
+    secret = Column(String(5000))
+    token = Column(String(5000))
+    created_at = Column(DateTime, default = datetime.now)
+    updated_on = Column(DateTime,
                         default = datetime.now,
                         onupdate = datetime.now)
+    
+    def __repr__(self):
+        return ("<Connection(id={0}, name={1})>"
+                .format(self.id,self.name))
 
+# Association table to define Many to Many relationship
+# between Flocks and birds
+bird_flock = Table('bird_flock',
+                   Base.metadata, 
+                   Column('flock_id', Integer, ForeignKey('flocks.id')),
+                   Column('bird_id', Integer, ForeignKey('birds.id'))
+                   )
+        
+class Flocks(Base):
+    __tablename__ = 'flocks'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default = datetime.now)
+    updated_on = Column(DateTime,
+                        default = datetime.now,
+                        onupdate = datetime.now)
+    birds = relationship('Birds', 
+                         backref = 'flocks',
+                         secondary=bird_flock)
+    def __repr__(self):
+        return ("<Flock(id={0}, name={1}>"
+                .format(self.id,self.name))
+    
+class Birds(Base):
+    __tablename__ = 'birds'
+    id = Column(Integer, primary_key=True)
+    platform_id = Column(String(1000), nullable=False)
+    platform_type = Column(Enum('Twitter'), nullable=False)
+    created_at = Column(DateTime, default = datetime.now)
+    updated_on = Column(DateTime,
+                        default = datetime.now,
+                        onupdate = datetime.now)
+    tweets = relationship('Tweets', backref='bird')
+    def __repr__(self):
+        return ("<Bird(id={0}, platform_id={1}, platform_type={2})>"
+                .format(self.id,self.platform_id, self.platform_type))
+    
 class Tweets(Base):
-    tweet_id = Column(String(255))
+    __tablename__ = 'tweets'
+    id = Column(Integer, primary_key=True)
+    bird_id = Column(Integer, ForeignKey('birds.id'), nullable=False)
     tweet_type = Column(String(255))
-    created_at = Column(DateTime())
+    # Derived from parent level attributes of JSON returned by 
+    # statuses/user_timeline
+    tweet_id = Column(String(255), nullable=False)
+    tweet_created_at = Column(DateTime)
     text = Column(String(255))
-    media_url = Column(String(255)) # extended_entities.media.media_url for images
-    # For video uploaded to twitter the media url contains a pic.
-    # The u'type' = u'video' and extended_entities.media.video_info.variants contains the video link
-    media_type = Column(String(255)) #extended_entities.media.video_info.variants.u'content_type' = 'video/mp4'[0].'url'
-    media_video_url = Column(String(255))
-    expanded_url = Column(String(255)) # entities.urls.expanded_url for video, html links
-    favorite_count = Column(Integer())
-    retweet_count = Column(Integer())
+    # Media uploaded to twitter
+    # Derived from extended_entities.media[].media_url
+    # In case of images it contains the url to the image
+    # In case of video contains a preview image of the video
+    media_urls = Column(String(1000))
+    # Derived from extended_entities.media[].type.
+    # Values are photo or video
+    media_types = Column(String(1000))
+    # Derived from extended_entities.media[].video_info.variants.
+    # content_type = 'video/mp4'[0].url
+    media_video_urls = Column(String(1000))
+    # Derived from entities.urls[].expanded_url. Contains links that 
+    # were part of the tweet
+    expanded_urls = Column(String(1000))
+    favorite_count = Column(Integer)
+    retweet_count = Column(Integer)
     user_id = Column(String(255))
-    user_screen_name = Column(String(255))
-
-    in_reply_to_tweet_id = Column(String(255)) # populated from u'in_reply_to_status_id_str
+    user_screen_name = Column(String(1000))
+    # Populated only for tweet_type = 'REPLY'
+    # Derived from u'in_reply_to_status_id_str
+    in_reply_to_tweet_id = Column(String(255)) 
     in_reply_to_user_id = Column(String(255))
-    in_reply_to_user_screen_name = Column(String(255))
-
+    in_reply_to_user_screen_name = Column(String(1000))
+    # Populated only for tweet_type = 'QUOTED RETWEET'
+    # Fields derived from "quoted_status" node within the json
     quoted_tweet_id = Column(String(255))
-    quoted_created_at = Column(DateTime()) #u'quoted_status'.u'created_at'
+    quoted_created_at = Column(DateTime)
     quoted_text = Column(String(255))
-    quoted_media_url = Column(String(255)) # quoted_status.entities.media.media_url
-    quoted_media_type = Column(String(255)) #quoted_status.extended_entities.media.video_info.variants.u'content_type' = 'video/mp4'[0].'url'
-    quoted_media_video_url = Column(String(255))
-    quoted_expanded_url = Column(String(255))
-    quoted_favorite_count = Column(Integer())
-    quoted_retweet_count = Column(Integer())
+    quoted_media_urls = Column(String(1000))
+    quoted_media_types = Column(String(1000))
+    quoted_media_video_urls = Column(String(1000))
+    quoted_expanded_urls = Column(String(1000))
+    quoted_favorite_count = Column(Integer)
+    quoted_retweet_count = Column(Integer)
     quoted_user_id = Column(String(255))
-    quoted_user_screen_name = Column(String(255))
-
+    quoted_user_screen_name = Column(String(1000))
+    # Populated only for tweet_type = 'RETWEET'
+    # Fields derived from "retweeted_status" node within the json
     retweet_tweet_id = Column(String(255))
-    retweet_created_at = Column(DateTime()) #u'retweeted_status'.u'created_at'
+    retweet_created_at = Column(DateTime)
     retweet_text = Column(String(255))
-    retweet_media_url = Column(String(255)) # retweeted_status.entities.media.media_url
-    retweet_media_type = Column(String(255)) #retweeted_status.extended_entities.media.video_info.variants.u'content_type' = 'video/mp4'[0].'url'
-    retweet_media_video_url = Column(String(255))
-    retweet_expanded_url = Column(String(255))
-    retweet_favorite_count = Column(Integer())
-    retweet_retweet_count = Column(Integer())
+    retweet_media_urls = Column(String(1000))
+    retweet_media_types = Column(String(1000))
+    retweet_media_video_urls = Column(String(1000))
+    retweet_expanded_urls = Column(String(1000))
+    retweet_favorite_count = Column(Integer)
+    retweet_retweet_count = Column(Integer)
     retweet_user_id = Column(String(255))
-    retweet_user_screen_name = Column(String(255))
-
-
+    retweet_user_screen_name = Column(String(1000))
+    created_at = Column(DateTime, default = datetime.now)
+    updated_on = Column(DateTime,
+                        default = datetime.now,
+                        onupdate = datetime.now)
+    def __repr__(self):
+        return ("<Tweet(id={0}, text={1}, user_screen_name={2})>"
+                .format(self.id,self.text, self.user_screen_name))     
 
 Base.metadata.create_all(engine)
